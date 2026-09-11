e66d4e34821c104de8d7df658ea2d15c5fec7e3f
abhatwar <akshay.hatwar5678@gmail.com>
2026-09-11 17:30:27 +0530

    third commit

---
+++ environment/production/iam-eks.tf
@@ -0,0 +1,40 @@
+# ========================================
+# EKS CLUSTER IAM ROLE
+# ========================================
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.project_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-eks-cluster-role"
+  }
+}
+
+# ========================================
+# EKS CLUSTER POLICY
+# ========================================
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role = aws_iam_role.eks_cluster.name
+
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
--- environment/production/variable.tf
+++ environment/production/variable.tf
@@ -31,3 +31,48 @@ variable "availability_zones" {
     "ap-south-1b"
   ]
 }
+
+
+
+# ========================================
+# EKS VARIABLES
+# ========================================
+
+variable "eks_cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "infrastructure-eks"
+}
+
+variable "eks_node_group_name" {
+  description = "EKS node group name"
+  type        = string
+  default     = "infrastructure-node-group"
+}
+
+variable "eks_node_instance_types" {
+  description = "EC2 instance types for EKS worker nodes"
+  type        = list(string)
+
+  default = [
+    "c7i-flex.large"
+  ]
+}
+
+variable "eks_node_desired_size" {
+  description = "Desired number of EKS worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "eks_node_min_size" {
+  description = "Minimum number of EKS worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "eks_node_max_size" {
+  description = "Maximum number of EKS worker nodes"
+  type        = number
+  default     = 3
+}
